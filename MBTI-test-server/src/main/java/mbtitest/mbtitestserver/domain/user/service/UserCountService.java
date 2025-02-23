3a67f96a95483e17cf2bf3c50f51a8894cd8cd07
wewqwew153 <wewqwew153@gmail.com>
2025-02-23 18:07:54 +0900

    feat: 유저 수 카운팅 기능 구현 - UserCountService.java 구현 - UserCountServiceImpl.java - UserCountResponse를 반환하는 increaseUserCount 구현

---
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/user/service/UserCountService.java
@@ -0,0 +1,8 @@
+package mbtitest.mbtitestserver.domain.user.service;
+
+import mbtitest.mbtitestserver.domain.user.model.dto.response.UserCountResponse;
+
+public interface UserCountService {
+
+	UserCountResponse increaseUserCount();
+}
---
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/user/service/UserCountServiceImpl.java
@@ -0,0 +1,25 @@
+package mbtitest.mbtitestserver.domain.user.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import lombok.RequiredArgsConstructor;
+import mbtitest.mbtitestserver.domain.user.model.dto.response.UserCountResponse;
+import mbtitest.mbtitestserver.domain.user.model.entity.UserCount;
+import mbtitest.mbtitestserver.domain.user.repository.UserCountRepository;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class UserCountServiceImpl implements UserCountService {
+
+	private final UserCountRepository userCountRepository;
+
+	@Override
+	public UserCountResponse increaseUserCount() {
+		UserCount userCount = userCountRepository.findById(1L).orElse(UserCount.builder().id(1L).build());
+		userCount.increaseUserCount();
+		userCountRepository.save(userCount);
+		return UserCountResponse.builder().userCount(userCount).build();
+	}
+}
