cc3313afdee04b5d0aa243f6070621e594924653
wewqwew153 <wewqwew153@gmail.com>
2025-02-23 14:52:12 +0900

    chore: 프로젝트 생성 init - 프로젝트 생성 - gitignore -> java | gradle | visualstudiocode | intelliJ | python

---
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/MbtiTestServerApplication.java
@@ -0,0 +1,13 @@
+package mbtitest.mbtitestserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MbtiTestServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MbtiTestServerApplication.class, args);
+	}
+
+}
---
+++ MBTI-test-server/src/test/java/mbtitest/mbtitestserver/MbtiTestServerApplicationTests.java
@@ -0,0 +1,13 @@
+package mbtitest.mbtitestserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MbtiTestServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
