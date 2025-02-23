4aa6d94e20d347bea25082cc51d6afcf61936d21
wewqwew153 <wewqwew153@gmail.com>
2025-02-23 18:06:24 +0900

    refactor: UserCountResponse.java Dto 의 빌더 파라미터 수정

--- MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/user/model/dto/response/UserCountResponse.java
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/user/model/dto/response/UserCountResponse.java
@@ -3,6 +3,7 @@ package mbtitest.mbtitestserver.domain.user.model.dto.response;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
+import mbtitest.mbtitestserver.domain.user.model.entity.UserCount;
 
 @Getter
 public class UserCountResponse {
@@ -12,8 +13,8 @@ public class UserCountResponse {
 
 	@Builder
 	UserCountResponse(
-		Long userCount
+		UserCount userCount
 	) {
-		this.userCount = userCount;
+		this.userCount = userCount.getUserCount();
 	}
 }
