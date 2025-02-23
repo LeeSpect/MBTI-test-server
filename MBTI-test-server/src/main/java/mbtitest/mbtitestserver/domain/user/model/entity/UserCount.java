fb69c442236f017e3a2de9f0ee0ee2a10f328db2
wewqwew153 <wewqwew153@gmail.com>
2025-02-23 17:35:33 +0900

    chore: UserCount Entity 구현

---
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/user/model/entity/UserCount.java
@@ -0,0 +1,32 @@
+package mbtitest.mbtitestserver.domain.user.model.entity;
+
+import static jakarta.persistence.GenerationType.*;
+import static lombok.AccessLevel.*;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@Builder
+@Table(name = "user_count")
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+public class UserCount {
+
+	@Id
+	@GeneratedValue(strategy = IDENTITY)
+	@Column(name = "user_count_id", columnDefinition = "BIGINT(20)")
+	private Long id;
+
+	@Builder.Default
+	@Column(name = "user_count", columnDefinition = "BIGINT(20) default 0")
+	private Long userCount = 0L;
+}
