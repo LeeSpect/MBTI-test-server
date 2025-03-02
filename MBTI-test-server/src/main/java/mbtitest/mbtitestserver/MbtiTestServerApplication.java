449378e4b04d4cd2df793e931a8403e7190e8819
wewqwew153 <wewqwew153@gmail.com>
2025-03-02 17:44:10 +0900

    Test: 배포 과정 로깅

--- MBTI-test-server/src/main/java/mbtitest/mbtitestserver/MbtiTestServerApplication.java
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/MbtiTestServerApplication.java
@@ -9,6 +9,11 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class MbtiTestServerApplication {
 
 	public static void main(String[] args) {
+		// 환경 변수 출력 (로그 확인용)
+		System.out.println("MYSQL_USER: " + System.getenv("MYSQL_USER"));
+		System.out.println("MYSQL_PASSWORD: " + System.getenv("MYSQL_PASSWORD"));
+		System.out.println("DOMAIN: " + System.getenv("DOMAIN"));
+
 		SpringApplication.run(MbtiTestServerApplication.class, args);
 	}
 
