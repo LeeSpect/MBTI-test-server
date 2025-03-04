8ad2cb0f785e06074907e934f3be926fd3f3248d
wewqwew153 <wewqwew153@gmail.com>
2025-03-04 18:50:58 +0900

    refactor: UserCountResponse.java의 userCount 필드에 JsonProperty로 totalCount 할당

--- MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/user/model/dto/response/UserCountResponse.java
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/user/model/dto/response/UserCountResponse.java
@@ -1,5 +1,6 @@
 package mbtitest.mbtitestserver.domain.user.model.dto.response;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Builder;
 import lombok.Getter;
@@ -9,6 +10,7 @@ import mbtitest.mbtitestserver.domain.user.model.entity.UserCount;
 public class UserCountResponse {
 
 	@Schema(description = "유저 수")
+	@JsonProperty("totalCount")
 	private final Long userCount;
 
 	@Builder
