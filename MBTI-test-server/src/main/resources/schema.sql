631ef7eef6a57155c71140c860b180072a73158c
wewqwew153 <wewqwew153@gmail.com>
2025-03-04 21:00:15 +0900

    refactor: DB 쿼리문 최종 수정

--- MBTI-test-server/src/main/resources/schema.sql
+++ MBTI-test-server/src/main/resources/schema.sql
@@ -1,13 +1,13 @@
 CREATE TABLE IF NOT EXISTS mbti_duo (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     mbti VARCHAR(50) NOT NULL,
     duo VARCHAR(50) NOT NULL,
     UNIQUE KEY (mbti, duo)
 );
-
--- MBTI별 인원수를 저장하는 테이블 생성
+#
+# -- MBTI별 인원수를 저장하는 테이블 생성
 CREATE TABLE IF NOT EXISTS mbti_count (
-    id INT AUTO_INCREMENT PRIMARY KEY,
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     mbti VARCHAR(4) NOT NULL,
-    count INT NOT NULL DEFAULT 0
+    member_count INT NOT NULL DEFAULT 0
 );
