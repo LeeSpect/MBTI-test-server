6801a51b48c8cf70f5c8c7b8580a39340c3e3337
wewqwew153 <wewqwew153@gmail.com>
2025-03-04 20:12:46 +0900

    add: MbtiDuo 엔티티 구현

---
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/travel/model/entity/MbtiDuo.java
@@ -0,0 +1,31 @@
+package mbtitest.mbtitestserver.domain.travel.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import static lombok.AccessLevel.PROTECTED;
+
+@Getter
+@Builder
+@Table(name = "mbti_duo")
+@NoArgsConstructor(access = PROTECTED)
+@AllArgsConstructor(access = PROTECTED)
+public class MbtiDuo {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "mbti", nullable = false)
+    private String mbti;
+
+    @Column(name = "duo", nullable = false)
+    private String duo;
+
+}
