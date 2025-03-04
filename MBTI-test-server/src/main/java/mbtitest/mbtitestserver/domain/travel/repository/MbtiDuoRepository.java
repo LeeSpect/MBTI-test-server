23ba14c8a97627d2488cfa982179189b668ba13c
wewqwew153 <wewqwew153@gmail.com>
2025-03-04 21:04:11 +0900

    add: MbtiDuoRepository.java 구현

---
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/domain/travel/repository/MbtiDuoRepository.java
@@ -0,0 +1,7 @@
+package mbtitest.mbtitestserver.domain.travel.repository;
+
+import mbtitest.mbtitestserver.domain.travel.model.entity.MbtiDuo;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MbtiDuoRepository extends JpaRepository<MbtiDuo, Long> {
+}
