ee3ed78775468f30c63ae99c9d5651815928f3b8
wewqwew153 <wewqwew153@gmail.com>
2025-03-12 14:58:09 +0900

    chore: 서버 api 호출 주소에 api 추가

--- MBTI-test-server/src/main/java/mbtitest/mbtitestserver/global/WebConfig.java
+++ MBTI-test-server/src/main/java/mbtitest/mbtitestserver/global/WebConfig.java
@@ -2,10 +2,19 @@ package mbtitest.mbtitestserver.global;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer.addPathPrefix("api", clazz -> {
+            String packageName = clazz.getPackageName();
+            return !packageName.contains("springdoc");
+        });
+    }
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
